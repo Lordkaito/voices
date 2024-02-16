e8e47adf91e04455d1246dddea003875e814569f
Lordkaito <cespedesisai88@gmail.com>
2024-02-16 22:09:39 +0100

    Update requests

--- requests.js
+++ requests.js
@@ -1,7 +1,3 @@
-// const voiceListUrl = "https://api.fakeyou.com/tts/list";
-// const audioRequestUrl = "https://api.fakeyou.com/tts/inference";
-// const pollAudioRequestUrl = "https://api.fakeyou.com/tts/job";
-// const audioStorageUrl = "https://storage.googleapis.com/vocodes-public";
 const voiceListUrl = process.env.VOICE_LIST_URL;
 const audioRequestUrl = process.env.AUDIO_REQUEST_URL;
 const pollAudioRequestUrl = process.env.POLL_AUDIO_URL;
@@ -18,56 +14,17 @@ function generateRandomString() {
   }
   return randomString;
 }
-// window.addEventListener("onEventReceived", function (obj) {
-//   if (obj.detail.event.value === "reset") {
-//     clearApiData();
-//     return;
-//   }
-
-//   const event = obj.detail.event;
-//   let listener = obj.detail.listener;
-
-//   if (event.isCommunityGift) return;
-
-//   if (listener === "cheer-latest") {
-//   }
-
-//   if (listener === "tip-latest") {
-//   }
-
-//   if (listener === "subscriber-latest") {
-//     console.log(event);
-//     if (event.message !== "") {
-//       const request = audioRequest(
-//         "TM:1hj3fftb6yrb",
-//         event.message,
-//         randomString
-//       );
-//       if (request.status) {
-//         const poll = pollAudioRequest(request.inferenceJobToken);
-//         if (poll.status) {
-//           requestAudio(poll.maybe_public_bucket_wav_audio_path);
-//         }
-//       }
-//     }
-//   }
-// });
 
 async function getVoiceList() {
   const response = await fetch(voiceListUrl);
   const data = await response.json();
   const titles = data.models.map((voice) => voice.title);
-  const chiquito = data.models.find(
-    (voice) => voice.title === "Chiquito de la Calzada. (Castillian Spanish.)"
-  );
-  console.log(chiquito);
-  // console.log(titles);
   return data;
 }
 
 async function audioRequest(
-  model = "TM:1hj3fftb6yrb",
-  text = "Hola, soy chiquito",
+  model,
+  text,
   randomId = randomString
 ) {
   const dataToSend = {
@@ -80,7 +37,6 @@ async function audioRequest(
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Cross-Origin-Opener-Policy": "same-origin",
     },
     body: JSON.stringify(dataToSend),
   }).then((response) => response.json());
@@ -98,16 +54,5 @@ async function pollAudioRequest(inferenceJobToken) {
   console.log(response);
   return response;
 }
-
-// async function requestAudio(
-//   audioPath = "/media/v/e/w/d/5/vewd5jyjy4byf8hqw4927b1c4vwadckn/fakeyou_vewd5jyjy4byf8hqw4927b1c4vwadckn.wav"
-// ) {
-//   const audioElement = new Audio(audioStorageUrl + audioPath);
-//   audioElement.play();
-// }
-// getVoiceList();
-// audioRequest();
-// pollAudioRequest();
-// requestAudio();
 
 module.exports = { audioRequest, pollAudioRequest, generateRandomString };
